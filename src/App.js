4aa4836f9b6ceb65744a18b25fcfbce524dc43de
sidharthmittal25 <sidharthmittal25@gmail.com>
2023-07-01 19:49:56 +0800

    better formatting and top 2 books shown

--- src/App.js
+++ src/App.js
@@ -37,23 +37,43 @@ function Store({ storeData, included }) {
   const storeImage = storeData.attributes.storeImage;
   const storeName = storeData.attributes.name;
   const storeRating = storeData.attributes.rating;
-  const topBooks = included.filter(item => item.type === 'books');
+  const topBooks = included
+    .filter(item => item.type === 'books')
+    .sort((a, b) => b.attributes.copiesSold - a.attributes.copiesSold)
+    .slice(0, 2);
 
   return (
     <div className="store">
-      <img src={storeImage} alt={storeName} />
-      <div>
-        <h2>{storeName}</h2>
-        <p>Rating: {storeRating}</p>
-        <p>Top Books:</p>
-        <ul>
-          {topBooks.map(book => (
-            <li key={book.id}>{book.attributes.name}</li>
-          ))}
-        </ul>
+      <div className="store-info">
+        <img src={storeImage} alt={storeName} />
+        <div>
+          <h2>{storeName}</h2>
+          <p>Rating: {storeRating}</p>
+        </div>
       </div>
+      <table>
+        <thead>
+          <tr>
+            <th>Book Name</th>
+            <th>Author</th>
+          </tr>
+        </thead>
+        <tbody>
+          {topBooks.map(book => (
+            <tr key={book.id}>
+              <td>{book.attributes.name}</td>
+              <td>{getAuthorName(book.relationships.author.data, included)}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
     </div>
   );
+}
+
+function getAuthorName(authorData, included) {
+  const author = included.find(item => item.type === 'authors' && item.id === authorData.id);
+  return author ? author.attributes.fullName : '';
 }
 
 export default App;
